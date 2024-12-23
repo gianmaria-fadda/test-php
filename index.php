0238278c6d0497509a7cc785b4b3fc89c26c6fb4
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-12-23 15:47:43 +0100

    fatta base esercizio + cambio titolo + creata struttura html

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test-php</title>
+</head>
+<body>
+    <form action="post">
+        <input type="text" name="text" placeholder="inserisci qui il testo...">
+        <button type="submit">Inserisci</button>
+    </form>
+</body>
+</html>
+
+<?php
+
+?>

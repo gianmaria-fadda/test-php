eff41235750fea8c217cc0f66cfad845db763897
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-12-26 11:22:42 +0100

    aggiunto vue.js a progetto + h1 + style

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test-php</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
 </head>
 <body>
+    <div id="app">
+        <h1>
+            {{ textH1 }}
+        </h1>
+    </div>
+
     <form action="index.php" method="post">
         <input type="text" name="testo" placeholder="inserisci qui il testo...">
         <input type="submit">
@@ -16,3 +23,22 @@
 </body>
 </html>
 
+<script>
+    const app = Vue.createApp({
+        data() {
+            return {
+                textH1: "Questa è una pagina di test"
+            };
+        }
+    });
+
+    app.mount('#app');
+</script>
+
+<style>
+    div {
+
+        display: flex;
+        justify-content: center;
+    };
+</style>
